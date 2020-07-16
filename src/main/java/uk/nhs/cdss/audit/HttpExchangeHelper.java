aea447019f6a2a3f0cc09c115109cffcc29c1f4a
Ionut-Claudiu (John) Herciu <john.herciu1@nhs.net>
2020-07-16 16:55:20 +0100

    gzip encoding and binary

--- src/main/java/uk/nhs/cdss/audit/HttpExchangeHelper.java
+++ src/main/java/uk/nhs/cdss/audit/HttpExchangeHelper.java
@@ -58,7 +58,6 @@ public class HttpExchangeHelper {
     var getAsGzip = getHeader(exchange, HttpHeaders.CONTENT_ENCODING)
         .map(contentEncoding -> gzipDecoder.decode(body, path, contentEncoding));
 
-    // TODO: gzip encoding and binary/non-text responses aren't mutually exclusive
     return getAsGzip
         .or(() -> getAsText)
         .orElseGet(() -> Base64.getEncoder().encodeToString(body));
