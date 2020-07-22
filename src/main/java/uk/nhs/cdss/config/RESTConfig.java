e108426c40008a1a5b3e89de8969d881dde527aa
Ionut-Claudiu (John) Herciu <john.herciu1@nhs.net>
2020-07-22 12:45:18 +0100

    Moved audit logic to common package.

---
+++ src/main/java/uk/nhs/cdss/config/RESTConfig.java
@@ -0,0 +1,16 @@
+package uk.nhs.cdss.config;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RESTConfig {
+
+  @Bean
+  public RestTemplate restTemplate(RestTemplateBuilder builder) {
+    return builder.build();
+  }
+
+}
